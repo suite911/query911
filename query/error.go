53a073e18c04629025121156d34307edb45a9a9b
Amy <amyadzuki@gmail.com>
2018-08-29 04:10:58 +0000

    Update error.go

--- query/error.go
+++ query/error.go
@@ -2,14 +2,19 @@ package query
 
 import "github.com/suite911/error911"
 
-type Error struct {
-	error911.Error
+type Error *error911.SError;
+
+func New(title string, cause error, msg ...interface{}) Error {
+	return error911.ImplNew(title, cause, msg...)
 }
 
-func NewError() *Error {
-	return new(Error).Init()
+func (err Error) Init(title string, cause error, msg ...interface{}) Error {
+	return error911.ImplInit(err, title, cause, msg...)
 }
 
-func (e *Error) Init() *Error {
-	return e
+func (err Error) New(title string, cause error, msg ...interface{}) Error {
+	return error911.ImplNewMethod(err, title, cause, msg...)
+}
+func (err *Error) Push(title string, cause error, msg ...interface{}) {
+	error911.ImplPush(err, title, cause, msg...)
 }
